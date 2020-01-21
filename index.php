79c90249e659305a5582680991714611cc3cba28
Montoon1 <montoon17@gmail.com>
2020-01-20 21:20:42 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+
+<html>
+
+<?php
+$name = 'Jeffrey Lynne';
+$email = 'lynnej1@nku.edu';
+$major = 'CIT';
+$year = '?';
+?>
+
+<header>
+<title> Introduction Page </title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+<h1> Introduction </h1>
+</header>
+
+<body>
+
+<div>
+<h5> Name: <?= $name ?> <br> 
+E-mail: <?= $email ?> <br>
+Major: <?= $major ?> <br>
+Year: <?= $year ?> <br> </h5>
+</div>
+
+<p>
+My main preoccupation is running the Esports club we currently have here at NKU. Other than that, I occasionally like to play golf here and there to show off to my friends. An Interesting fact about me... Is that I have taught English in China for 3 months (not school affiliated.)
+</p>
+
+<img src="https://scontent-iad3-1.xx.fbcdn.net/v/t31.0-8/p720x720/22181635_1719287584809596_6766229527595710907_o.jpg?_nc_cat=104&_nc_ohc=jsLz6MGWFBkAX_vML1q&_nc_ht=scontent-iad3-1.xx&_nc_tp=1002&oh=274eb14d4bd85013949c5c542b5084ed&oe=5ECC568E">
+<p>
+I am taking this class because I've only heard great things about the professor! Also something something Major... <br>
+
+I am currently getting back into coding. Side note, Calculus took me 4 attempts :^)
+</p>
+</body>
+</html>
+
